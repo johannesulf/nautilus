dfff2265831f16c4c71083537cae30ce2581ebaf
Johannes U. Lange <julange.astro@pm.me>
2022-12-16 13:32:50 -0500

    updated lux environment

--- paper/init.sh
+++ paper/init.sh
@@ -1,9 +1,7 @@
 module load python
-module load openmpi
 module load hdf5
 
 source env/bin/activate
 export PYTHONPATH="${PYTHONPATH}:/data/groups/leauthaud/jolange/Nautilus/"
-export SPS_HOME="/data/groups/leauthaud/jolange/Nautilus/paper/fsps"
 
 export HDF5_USE_FILE_LOCKING='FALSE'
