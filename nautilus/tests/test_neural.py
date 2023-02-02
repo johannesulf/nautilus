bdccfc633fa189cf63e5c0c288e44b3b167dc341
Johannes U. Lange <julange.astro@pm.me>
2023-02-02 16:30:10 -0500

    updated tests

--- nautilus/tests/test_neural.py
+++ nautilus/tests/test_neural.py
@@ -10,6 +10,6 @@ def test_neural_network_emulator():
     neural_network_kwargs = {
         'alpha': 0, 'tol': 0, 'max_iter': 1000, 'n_iter_no_change': 50,
         'hidden_layer_sizes': (128, 128, 128)}
-    emu = neural.NeuralNetworkEmulator(
+    emu = neural.NeuralNetworkEmulator.train(
         x, y, neural_network_kwargs=neural_network_kwargs)
     assert np.sqrt(np.mean((y - emu.predict(x))**2)) < 0.1 * np.std(y)
